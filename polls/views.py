cb3257ffd8d30237a42274873f4216358942f1b0
MrKonrados <konrad.slizewski@gmail.com>
2017-06-11 11:48:29 +0200

    index page

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello World. This is Index page")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list':latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
+
+def detail(request, question_id):
+    return HttpResponse("Detail page %s" % question_id)
+
+
+def results(request, question_id):
+    response = "Results page of %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Voting page of %s" % question_id)
